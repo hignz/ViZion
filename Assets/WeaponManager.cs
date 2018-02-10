7c612011206b0de099407b9322cc5d4315084f34
hignz <higz-14@live.com>
2018-02-10 20:00:36 +0000

    Cleanup

--- Assets/WeaponManager.cs
+++ Assets/WeaponManager.cs
@@ -3,17 +3,17 @@
 public class WeaponManager : MonoBehaviour {
 
     public GameObject currentWeapon;
-    //public Weapon weapon;
+    public Weapon weapon;
 
 	void Start () {
-        //weapon = currentWeapon.GetComponent<Weapon>();
-        //GetComponent<SpriteRenderer>().sprite = weapon.sprite;
+        weapon = currentWeapon.GetComponent<Weapon>();
+        GetComponent<SpriteRenderer>().sprite = weapon.sprite;
 	}
 
     public void PickupWeapon(GameObject weapon)
     {
         currentWeapon = weapon;
-        //weapon = currentWeapon.GetComponent<Weapon>().gameObject;
+        weapon = currentWeapon.GetComponent<Weapon>().gameObject;
         GetComponent<SpriteRenderer>().sprite = weapon.GetComponent<SpriteRenderer>().sprite;
     }
 }
