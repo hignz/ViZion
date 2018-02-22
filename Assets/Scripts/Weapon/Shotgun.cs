7c7aa5509e5b761c3c684a3c73ba840ecea9c286
hignz <higz-14@live.com>
2018-02-22 00:05:38 +0000

    More work on weapon system, hub scene improvments, added particle systems for backgrounds

--- Assets/Scripts/Weapon/Shotgun.cs
+++ Assets/Scripts/Weapon/Shotgun.cs
@@ -1,8 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Shotgun : Weapon {
+
+    void Start()
+    {
+        ammo = 6;
+    }
 
     public override void Fire()
     {
