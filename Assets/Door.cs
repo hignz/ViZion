5f7ab1f74373acfb3d3a32726a2bfed906bb0679
hignz <higz-14@live.com>
2018-04-16 19:55:20 +0100

    fixes

---
+++ Assets/Door.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision other)
+    {
+        string tag = other.gameObject.tag;
+        Debug.Log("outside");
+
+        if (tag.Equals("Enemy") && Vector2.Distance(transform.position, FindObjectOfType<Player>().transform.position) <= 1f)
+        {
+            Debug.Log("inside");
+            Destroy(other.gameObject);
+        }
+    }
+}
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -2,11 +2,24 @@
 
 public class Player : MonoBehaviour {
 
+    public Sprite equip, unequipped;
+
     void Start () {
-		
+		 
 	}
 	
 	void Update () {
 		
-	} 
+	}
+
+    public void SwapToEquipSprite()
+    {
+        GetComponent<SpriteRenderer>().sprite = equip;
+    }
+
+    public void SwapToUnequippedSprite()
+    {
+        GetComponent<SpriteRenderer>().sprite = unequipped;
+    }
+
 }
