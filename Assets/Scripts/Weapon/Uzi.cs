355411433028642d27a3385acf38022d54312ad2
hignz <higz-14@live.com>
2018-04-25 14:09:07 +0100

    stuff

--- Assets/Scripts/Weapon/Uzi.cs
+++ Assets/Scripts/Weapon/Uzi.cs
@@ -12,28 +12,10 @@ public class Uzi : Weapon
 
     public override void Fire()
     {
-        //Vector2 mousePos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
-        //Vector2 firePos = new Vector2(firePoint.position.x, firePoint.position.y);
-        //RaycastHit2D hit = Physics2D.Raycast(firePos, mousePos - firePos, 100, whatToHit);
-        //Debug.DrawLine(firePos, (mousePos - firePos) * 100, Color.cyan, 4);
-
-        ////// Show bullet trail
-        //Instantiate(BulletLine, firePoint.position, firePoint.rotation);
-        //PlaySoundEffect();
-
-        //if (hit.collider != null)
-        //{
-        //    Debug.DrawLine(firePos, hit.point, Color.red, 6);
-        //    Debug.Log("I hit " + hit.collider.name);
-
-        //    CheckHit(hit);
-
         GameObject bullet = Instantiate(BulletLine, firePoint.position, firePoint.rotation);
         bullet.transform.Rotate(0, 0, Random.Range(-1, 1));
 
         PlaySoundEffect();
-
-        //}
     }
 
     void CheckHit(RaycastHit2D hit)
