d764f1dacee1c16a42f110763dbac1488a4717b3
hignz <higz-14@live.com>
2018-04-26 16:29:43 +0100

    Added ability to zoom out in ship
    There are 4 zoom levels, min, level1, level2 and max. Camera smoothly interpolates between these after pressing tab

---
+++ Assets/CameraZoom.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+
+    public float zoomLevelMin, zoomLevel2, zoomLevel3, zoomLevelMax;
+    private Camera PlayerCamera;
+    public float zoomSpeed = 0.5f;
+
+    private int currentLevel = 1;
+
+    // Use this for initialization
+    void Start()
+    {
+        PlayerCamera = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            currentLevel++;
+
+            if (currentLevel > 4)
+            {
+                currentLevel = 1;
+            }
+
+            Debug.Log(currentLevel);
+        }
+    }
+
+    void LateUpdate()
+    {
+        switch (currentLevel)
+        {
+            case 1:
+                PlayerCamera.orthographicSize = Mathf.Lerp(PlayerCamera.orthographicSize, zoomLevelMin, Time.deltaTime * zoomSpeed);
+                break;
+            case 2:
+                PlayerCamera.orthographicSize = Mathf.Lerp(PlayerCamera.orthographicSize, zoomLevel2, Time.deltaTime * zoomSpeed);
+                break;
+            case 3:
+                PlayerCamera.orthographicSize = Mathf.Lerp(PlayerCamera.orthographicSize, zoomLevel3, Time.deltaTime * zoomSpeed);
+                break;
+            case 4:
+                PlayerCamera.orthographicSize = Mathf.Lerp(PlayerCamera.orthographicSize, zoomLevelMax, Time.deltaTime * zoomSpeed);
+                break;
+            default:
+                break;
+        }
+
+    }
+}
