ddf6f862afbf01e013e42e737c6238c4b2e35104
hignz <higz-14@live.com>
2018-04-28 19:43:15 +0100

    Misc

--- Assets/Scripts/Weapon/Bullet.cs
+++ Assets/Scripts/Weapon/Bullet.cs
@@ -1,26 +1,21 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
-using TMPro;
 using System.Collections;
+using TMPro;
 
 public class Bullet : MonoBehaviour
 {
-    public int speed = 200;
+    private bool gameOver = false;
     private Rigidbody2D rb;
 
+    public int speed = 200;
     public TextMeshProUGUI restartPromt;
-
-    private bool gameOver = false;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
 
-        GetComponent<SpriteRenderer>().transform.localScale.Set(1, 1 + Random.Range(-2f, 2f), 1);
-
         Destroy(gameObject, 1.2f);
-
-
     }
 
     void Update()
@@ -49,7 +44,9 @@ public class Bullet : MonoBehaviour
         {
             other.gameObject.GetComponent<Enemy>().Die();
 
-            FindObjectOfType<LevelManager>().RemoveEnemy();
+            //FindObjectOfType<LevelManager>().RemoveEnemy();
+
+            LevelManager.AddScore(100);
         }
         else if (tag.Equals("Player"))
         {
@@ -57,15 +54,15 @@ public class Bullet : MonoBehaviour
 
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
 
-            StartCoroutine(LateCall());
+           // StartCoroutine(LateCall());
         }
     }
 
     IEnumerator LateCall()
     {
-        yield return new WaitForSeconds(1.5f);
+        yield return new WaitForSeconds(2f);
 
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Single);
     }
 
 }
